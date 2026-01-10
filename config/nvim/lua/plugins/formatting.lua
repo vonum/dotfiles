30b8926a3278304020bd0655bdf517cd20ee9ecb
vonum <vonum.mk@gmail.com>
2026-01-10 23:13:00 +0100

    Update lsp config.

--- config/nvim/lua/plugins/formatting.lua
+++ config/nvim/lua/plugins/formatting.lua
@@ -1,37 +1,37 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
-    conform.setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        golang = { "gofmt", "goimports" },
-        python = { "isort", "black" },
-        rust = { "rustfmt" },
-        elixir = { "mix" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        terraform = { "terraform_fmt" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 500,
-      },
-    })
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				golang = { "gofmt", "goimports" },
+				python = { "isort", "black" },
+				rust = { "rustfmt" },
+				elixir = { "mix" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				terraform = { "terraform_fmt" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
 
-    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-      conform.format({
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 500,
-      })
-    end, { desc = "Format file or range (in visual mode)" })
-  end,
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -1,148 +1,184 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    { "mason-org/mason.nvim", opts = {} },
-    {
-      "mason-org/mason-lspconfig.nvim",
-      opts = {
-        ensure_installed = {
-          "lua_ls",
-          "gopls",
-          "pyright",
-          "rust_analyzer",
-          "elixirls",
-          "ts_ls",
-          -- "rubocop",
-          "solc",
-          "marksman",
-          "yamlls",
-          "terraformls",
-        },
-        automatic_enable = false,
-      },
-      dependencies = { "mason-org/mason.nvim", "neovim/nvim-lspconfig" }
-    },
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  config = function()
-    local lspconfig = require("lspconfig")
-    local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{ "mason-org/mason.nvim", opts = {} },
+		{
+			"mason-org/mason-lspconfig.nvim",
+			opts = {
+				ensure_installed = {
+					"lua_ls",
+					"gopls",
+					"pyright",
+					"rust_analyzer",
+					"elixirls",
+					"ts_ls",
+					-- "rubocop",
+					"solc",
+					"marksman",
+					"yamlls",
+					"terraformls",
+				},
+				automatic_enable = false,
+			},
+			dependencies = { "mason-org/mason.nvim", "neovim/nvim-lspconfig" },
+		},
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	config = function()
+		local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    local handlers = {
-      ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-      ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
-    }
+		local handlers = {
+			["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+			["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
+		}
 
-    lspconfig.gopls.setup({
-      capabilities = lsp_capabilities,
-      handlers = handlers
-    })
-    lspconfig.pyright.setup({
-      on_init = function(client)
-        local poetry_env = vim.fn.trim(vim.fn.system("poetry env info -p"))
-        client.config.settings.python.pythonPath = poetry_env .. "/bin/python"
-        client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-        return true
-      end,
-      capabilities = lsp_capabilities,
-      handlers = handlers,
-      -- python = {
-      --   pythonPath = vim.fn.trim(vim.fn.system("uv run which python")),
-      -- },
-    })
-    lspconfig.rust_analyzer.setup({
-      capabilities = lsp_capabilities,
-      handlers = handlers
-    })
-    lspconfig.elixirls.setup({
-      capabilities = lsp_capabilities,
-      handlers = handlers
-    })
-    lspconfig.solc.setup({
-      capabilities = lsp_capabilities,
-      handlers = handlers
-    })
-    lspconfig.rubocop.setup({
-      capabilities = lsp_capabilities,
-      handlers = handlers
-    })
-    lspconfig.ts_ls.setup({
-      capabilities = lsp_capabilities,
-      handlers = handlers
-    })
-    lspconfig.marksman.setup({
-      capabilities = lsp_capabilities,
-      handlers = handlers
-    })
+		vim.lsp.config.gopls = {
+			capabilities = lsp_capabilities,
+			handlers = handlers,
+		}
+		vim.lsp.enable("gopls")
 
-    lspconfig.terraformls.setup({
-      capabilities = lsp_capabilities,
-      handlers = handlers,
-      init_options = {
-        terraform = {
-          path = "/opt/homebrew/bin/terraform"
-        }
-      }
-    })
+		vim.lsp.config.pyright = {
+			-- on_init = function(client)
+			--   local poetry_env = vim.fn.trim(vim.fn.system("poetry env info -p"))
+			--   client.config.settings.python.pythonPath = poetry_env .. "/bin/python"
+			--   client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+			--   return true
+			-- end,
+			capabilities = lsp_capabilities,
+			handlers = handlers,
+			-- python = {
+			--   pythonPath = vim.fn.trim(vim.fn.system("uv run which python")),
+			-- },
+			python = {
+				pythonPath = function()
+					local function cmd(cmd)
+						local out = vim.fn.system(cmd)
+						return vim.v.shell_error == 0 and vim.fn.trim(out) or nil
+					end
 
-    lspconfig.lua_ls.setup({
-      capabilities = lsp_capabilities,
-      handlers = handlers,
-      settings = {
-        Lua = {
-          diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = { "vim" },
-          },
-        },
-      },
-    })
+					local poetry = cmd("poetry env info -p")
+					if poetry and poetry ~= "" then
+						return poetry .. "/bin/python"
+					end
 
-    lspconfig.yamlls.setup({
-      capabilities = lsp_capabilities,
-      handlers = handlers,
-      settings = {
-        yaml = {
-          schemas = {
-            kubernetes = "*.yaml",
-            ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-            ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-            ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
-            "*docker-compose*.{yml,yaml}",
-          },
-        }
-      }
-    })
+					local uv = cmd("uv run which python")
+					if uv and uv ~= "" then
+						return uv
+					end
 
-    -- local custom_map = function(type, key, value)
-    --   -- vim.api.nvim_buf_set_keymap(0,type,key,value,{noremap = true, silent = true})
-    --   vim.keymap.set(type, key, value, { noremap = true, silent = true })
-    -- end
-    -- custom_map("n", "<leader>=", "<cmd> lua vim.lsp.buf.formatting()<cr>")
+					return vim.fn.exepath("python3") or "python"
+				end,
+			},
+		}
+		vim.lsp.enable("pyright")
 
-    vim.api.nvim_create_autocmd('LspAttach', {
-      desc = 'LSP actions',
-      callback = function()
-        local bufmap = function(mode, lhs, rhs)
-          local opts = { buffer = true }
-          vim.keymap.set(mode, lhs, rhs, opts)
-        end
+		vim.lsp.config.rust_analyzer = {
+			capabilities = lsp_capabilities,
+			handlers = handlers,
+		}
+		vim.lsp.enable("rust_analyzer")
 
-        bufmap('n', 'K', vim.lsp.buf.hover)
-        bufmap('n', 'gd', vim.lsp.buf.definition)
-        bufmap('n', 'gD', vim.lsp.buf.declaration)
-        bufmap('n', 'gi', vim.lsp.buf.implementation)
-        bufmap('n', 'gt', vim.lsp.buf.type_definition)
+		vim.lsp.config.elixirls = {
+			capabilities = lsp_capabilities,
+			handlers = handlers,
+		}
+		vim.lsp.enable("elixirls")
 
-        bufmap('n', 'gr', vim.lsp.buf.references)
-        bufmap('n', 'gs', vim.lsp.buf.signature_help)
-        bufmap("n", "gai", vim.lsp.buf.incoming_calls)
-        bufmap("n", "gao", vim.lsp.buf.outgoing_calls)
+		vim.lsp.config.solc = {
+			capabilities = lsp_capabilities,
+			handlers = handlers,
+		}
+		vim.lsp.enable("solc")
 
-        bufmap('n', 'gl', vim.diagnostic.open_float)
-        bufmap('n', '[d', vim.diagnostic.goto_prev)
-        bufmap('n', ']d', vim.diagnostic.goto_next)
-      end
-    })
-  end,
+		vim.lsp.config.rubocop = {
+			capabilities = lsp_capabilities,
+			handlers = handlers,
+		}
+		vim.lsp.enable("rubocop")
+
+		vim.lsp.config.ts_ls = {
+			capabilities = lsp_capabilities,
+			handlers = handlers,
+		}
+		vim.lsp.enable("ts_ls")
+
+		vim.lsp.config.marksman = {
+			capabilities = lsp_capabilities,
+			handlers = handlers,
+		}
+		vim.lsp.enable("marksman")
+
+		vim.lsp.config.terraformls = {
+			capabilities = lsp_capabilities,
+			handlers = handlers,
+			init_options = {
+				terraform = {
+					path = "/opt/homebrew/bin/terraform",
+				},
+			},
+		}
+		vim.lsp.enable("terraformls")
+
+		vim.lsp.config.lua_ls = {
+			capabilities = lsp_capabilities,
+			handlers = handlers,
+			settings = {
+				Lua = {
+					diagnostics = {
+						-- Get the language server to recognize the `vim` global
+						globals = { "vim" },
+					},
+				},
+			},
+		}
+		vim.lsp.enable("lua_ls")
+
+		vim.lsp.config.yamlls = {
+			capabilities = lsp_capabilities,
+			handlers = handlers,
+			settings = {
+				yaml = {
+					schemas = {
+						kubernetes = "*.yaml",
+						["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+						["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+						["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+					},
+				},
+			},
+		}
+		vim.lsp.enable("yamlls")
+
+		-- local custom_map = function(type, key, value)
+		--   -- vim.api.nvim_buf_set_keymap(0,type,key,value,{noremap = true, silent = true})
+		--   vim.keymap.set(type, key, value, { noremap = true, silent = true })
+		-- end
+		-- custom_map("n", "<leader>=", "<cmd> lua vim.lsp.buf.formatting()<cr>")
+
+		vim.api.nvim_create_autocmd("LspAttach", {
+			desc = "LSP actions",
+			callback = function()
+				local bufmap = function(mode, lhs, rhs)
+					local opts = { buffer = true }
+					vim.keymap.set(mode, lhs, rhs, opts)
+				end
+
+				bufmap("n", "K", vim.lsp.buf.hover)
+				bufmap("n", "gd", vim.lsp.buf.definition)
+				bufmap("n", "gD", vim.lsp.buf.declaration)
+				bufmap("n", "gi", vim.lsp.buf.implementation)
+				bufmap("n", "gt", vim.lsp.buf.type_definition)
+
+				bufmap("n", "gr", vim.lsp.buf.references)
+				bufmap("n", "gs", vim.lsp.buf.signature_help)
+				bufmap("n", "gai", vim.lsp.buf.incoming_calls)
+				bufmap("n", "gao", vim.lsp.buf.outgoing_calls)
+
+				bufmap("n", "gl", vim.diagnostic.open_float)
+				bufmap("n", "[d", vim.diagnostic.goto_prev)
+				bufmap("n", "]d", vim.diagnostic.goto_next)
+			end,
+		})
+	end,
 }
