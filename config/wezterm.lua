e251dbb37b2d5335bdf006587cd53639e9cc1987
vonum <vonum.mk@gmail.com>
2026-01-11 00:38:22 +0100

    Add background images for wezterm.

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -10,11 +10,24 @@ config.font_size = 13
 config.color_scheme = "kanagawabones"
 
 config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
-
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
+config.window_decorations = "TITLE | RESIZE"
 
 config.default_cursor_style = "BlinkingBar"
+
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 15
+config.background = {
+	{
+		source = {
+			File = os.getenv("HOME") .. "/dotfiles/assets/bg4.jpeg",
+		},
+		horizontal_align = "Center",
+		vertical_align = "Middle",
+		opacity = 0.25,
+	},
+}
+
+config.check_for_updates = true
+config.check_for_updates_interval_seconds = 86400
 
 return config
