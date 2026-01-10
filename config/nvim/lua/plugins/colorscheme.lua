c4fe535ab05171d75607804c4270b59ff88a6be6
vonum <vonum.mk@gmail.com>
2026-01-10 23:50:57 +0100

    Add transparency to nvim.

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,34 @@
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("rose-pine")
-  end,
+	"rose-pine/neovim",
+	name = "rose-pine",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("rose-pine").setup({
+			-- disable_background = true,
+			-- disable_floating_background = true,
+		})
+		vim.cmd.colorscheme("rose-pine")
+
+		-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none" })
+
+		-- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "none" })
+
+		-- vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
+	end,
 }
 
 -- return {
--- config/nvim/lua/plugins/transparent.lua
+++ config/nvim/lua/plugins/transparent.lua
@@ -1,8 +1,9 @@
 return {
-  "xiyaowong/transparent.nvim",
-  lazy = true,
-  -- config = function ()
-  --   :TransparentToggle
-  --   vim.keymap.set("n", "<leader>bg", ":TransparentToggle<cr>", { silent = true })
-  -- end
+	"xiyaowong/transparent.nvim",
+	lazy = false,
+	config = function()
+		require("transparent").clear_prefix("lualine")
+		require("transparent").clear_prefix("Telescope")
+		vim.keymap.set("n", "<leader>bg", ":TransparentToggle<cr>", { silent = true })
+	end,
 }
