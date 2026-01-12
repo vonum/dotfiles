29914cd3925150a57f14fc9f3027691393d5e650
vonum <vonum.mk@gmail.com>
2026-01-12 11:41:54 +0100

    Add splits for wezterm.

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 local config = wezterm.config_builder()
 
@@ -10,7 +11,7 @@ config.font_size = 13
 config.color_scheme = "kanagawabones"
 
 config.enable_tab_bar = false
-config.window_decorations = "TITLE | RESIZE"
+config.window_decorations = "RESIZE"
 
 config.default_cursor_style = "BlinkingBar"
 
@@ -34,32 +35,36 @@ config.window_padding = {
 	right = 3,
 }
 
+config.inactive_pane_hsb = {
+	saturation = 0.6,
+	brightness = 0.7,
+}
+
 config.keys = {
-	{
-		key = "m",
-		mods = "OPT",
-		action = wezterm.action.ToggleFullScreen,
-	},
-	{
-		key = "RightArrow",
-		mods = "OPT",
-		action = wezterm.action.SendString("\x1bf"), -- forward one word
-	},
-	{
-		key = "LeftArrow",
-		mods = "OPT",
-		action = wezterm.action.SendString("\x1bb"), -- back one word
-	},
-	{
-		key = "RightArrow",
-		mods = "SHIFT|OPT",
-		action = wezterm.action.SendString("\x05"), -- end of line
-	},
-	{
-		key = "LeftArrow",
-		mods = "SHIFT|OPT",
-		action = wezterm.action.SendString("\x01"), -- beginning of line
-	},
+	{ key = "m", mods = "OPT", action = wezterm.action.ToggleFullScreen },
+	-- forward one word
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action.SendString("\x1bf") },
+	-- back one word
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action.SendString("\x1bb") },
+	-- end of line
+	{ key = "RightArrow", mods = "SHIFT|OPT", action = wezterm.action.SendString("\x05") },
+	-- beginning of line
+	{ key = "LeftArrow", mods = "SHIFT|OPT", action = wezterm.action.SendString("\x01") },
+
+	-- Panes
+	{ key = "S", mods = "OPT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "s", mods = "OPT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "w", mods = "OPT", action = act.CloseCurrentPane({ confirm = true }) },
+	{ key = "h", mods = "OPT", action = act.ActivatePaneDirection("Left") },
+	{ key = "l", mods = "OPT", action = act.ActivatePaneDirection("Right") },
+	{ key = "k", mods = "OPT", action = act.ActivatePaneDirection("Up") },
+	{ key = "j", mods = "OPT", action = act.ActivatePaneDirection("Down") },
+
+	{ key = "M", mods = "OPT", action = act.TogglePaneZoomState },
+	{ key = "h", mods = "SHIFT|OPT", action = act.AdjustPaneSize({ "Left", 3 }) },
+	{ key = "l", mods = "SHIFT|OPT", action = act.AdjustPaneSize({ "Right", 3 }) },
+	{ key = "k", mods = "SHIFT|OPT", action = act.AdjustPaneSize({ "Up", 3 }) },
+	{ key = "j", mods = "SHIFT|OPT", action = act.AdjustPaneSize({ "Down", 3 }) },
 }
 
 config.check_for_updates = true
