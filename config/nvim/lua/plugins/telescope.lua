46b3b2a9000246cd22eb3975cb6bd954674516ec
vonum <vonum.mk@gmail.com>
2026-02-18 16:57:32 +0100

    Add mapping for searching keymaps.

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,23 +1,24 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.8",
-  config = function()
-    local builtin = require('telescope.builtin')
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	config = function()
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+		vim.keymap.set("n", "<leader>km", builtin.keymaps, { desc = "Telescope keymaps" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 
-    require("telescope").setup {
-      defaults = {
-        file_ignore_patterns = {
-          ".git/",
-          "node_modules/",
-          "target",
-          "__pycache__/*"
-        }
-      }
-    }
-  end,
-  dependencies = { "nvim-lua/plenary.nvim" }
+		require("telescope").setup({
+			defaults = {
+				file_ignore_patterns = {
+					".git/",
+					"node_modules/",
+					"target",
+					"__pycache__/*",
+				},
+			},
+		})
+	end,
+	dependencies = { "nvim-lua/plenary.nvim" },
 }
